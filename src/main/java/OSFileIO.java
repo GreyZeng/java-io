b921f4b53af59bedc70c43a8cf0aadcdd6bf0f28
GreyZeng <410486047@qq.com>
2021-06-10 20:26:30 +0800

    init project

---
+++ src/main/java/OSFileIO.java
@@ -0,0 +1,43 @@
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+
+public class OSFileIO {
+
+
+    public static void main(String[] args) throws Exception {
+        switch (args[0]) {
+            case "0":
+                testBasicFileIO();
+                break;
+            case "1":
+                testBufferedFileIO();
+                break;
+
+            default:
+
+        }
+    }
+
+
+    //最基本的file写
+    static byte[] data = "123456789\n".getBytes();
+    static String path = "/data/io/out.txt";
+
+    public static void testBasicFileIO() throws Exception {
+        File file = new File(path);
+        FileOutputStream out = new FileOutputStream(file);
+        while (true) {
+            out.write(data);
+        }
+    }
+
+    public static void testBufferedFileIO() throws Exception {
+        File file = new File(path);
+        BufferedOutputStream out = new BufferedOutputStream(new FileOutputStream(file));
+        while (true) {
+            out.write(data);
+        }
+    }
+}
+
