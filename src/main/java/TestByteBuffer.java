bdffbae3fd71031a581736a5f641b5ef77524681
GreyZeng <410486047@qq.com>
2021-06-12 11:17:14 +0800

    byte buffer

---
+++ src/main/java/TestByteBuffer.java
@@ -0,0 +1,34 @@
+import java.nio.ByteBuffer;
+
+public class TestByteBuffer {
+    public static void main(String[] args) {
+        ByteBuffer buffer = ByteBuffer.allocateDirect(1024);
+
+        System.out.println("mark: " + buffer);
+
+        buffer.put("123".getBytes());
+
+        System.out.println("-------------put:123......");
+        System.out.println("mark: " + buffer);
+
+        buffer.flip();   //读写交替
+
+        System.out.println("-------------flip......");
+        System.out.println("mark: " + buffer);
+
+        buffer.get();
+
+        System.out.println("-------------get......");
+        System.out.println("mark: " + buffer);
+
+        buffer.compact();
+
+        System.out.println("-------------compact......");
+        System.out.println("mark: " + buffer);
+
+        buffer.clear();
+
+        System.out.println("-------------clear......");
+        System.out.println("mark: " + buffer);
+    }
+}
--- src/main/java/org/snippets/App.java
+++
@@ -1,8 +0,0 @@
-package org.snippets;
-
-
-public class App {
-    public static void main(String[] args) {
-        
-    }
-}
