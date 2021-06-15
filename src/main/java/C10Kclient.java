f76e2ca19117563a87a83e59c262629ff2877e65
GreyZeng <410486047@qq.com>
2021-06-15 19:57:23 +0800

    BIO to NIO

---
+++ src/main/java/C10Kclient.java
@@ -0,0 +1,38 @@
+
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.channels.SocketChannel;
+import java.util.LinkedList;
+
+
+public class C10Kclient {
+
+    public static void main(String[] args) {
+        LinkedList<SocketChannel> clients = new LinkedList<>();
+        InetSocketAddress serverAddr = new InetSocketAddress("192.168.205.138", 9090);
+        for (int i = 10000,j = 10001; i < 65000; i+=2,j+=2) {
+            try {
+                SocketChannel client1 = SocketChannel.open();
+                SocketChannel client2 = SocketChannel.open();
+                client1.bind(new InetSocketAddress("192.168.205.1", i )); 
+                client1.connect(serverAddr);
+                clients.add(client1);
+
+                client2.bind(new InetSocketAddress("192.168.205.1", j));
+                //  192.168.110.100：10000  192.168.150.11：9090
+                client2.connect(serverAddr);
+                clients.add(client2);
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        System.out.println("clients "+ clients.size());
+        try {
+            System.in.read();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/main/java/SocketServerNIOTest.java
@@ -0,0 +1,47 @@
+import java.net.InetSocketAddress; 
+import java.nio.ByteBuffer;
+import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.SocketChannel;
+import java.util.LinkedList;
+
+public class SocketServerNIOTest {
+
+
+    public static void main(String[] args) throws Exception {
+
+        LinkedList<SocketChannel> clients = new LinkedList<>();
+
+        ServerSocketChannel ss = ServerSocketChannel.open();  //服务端开启监听：接受客户端
+        ss.bind(new InetSocketAddress(9090));
+        ss.configureBlocking(false); 
+
+        while (true) {
+        	
+            SocketChannel client = ss.accept();
+            if (client == null) {
+             
+            } else {
+                client.configureBlocking(false); //重点  socket（服务端的listen socket<连接请求三次握手后，往我这里扔，我去通过accept 得到  连接的socket>，连接socket<连接后的数据读写使用的> ）
+                int port = client.socket().getPort();
+                System.out.println("client..port: " + port);
+                clients.add(client);
+            }
+
+            ByteBuffer buffer = ByteBuffer.allocateDirect(4096); 
+            //遍历已经链接进来的客户端能不能读写数据
+            for (SocketChannel c : clients) {  
+                int num = c.read(buffer);  
+                if (num > 0) {
+                    buffer.flip();
+                    byte[] aaa = new byte[buffer.limit()];
+                    buffer.get(aaa);
+
+                    String b = new String(aaa);
+                    System.out.println(c.socket().getPort() + " : " + b);
+                    buffer.clear();
+                }
+            }
+        }
+    }
+
+}
