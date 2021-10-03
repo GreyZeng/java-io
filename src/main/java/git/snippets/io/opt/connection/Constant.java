4356fba8975d5e5c8744d48df62ee829b5e66e79
GreyZeng <410486047@qq.com>
2021-10-03 13:55:07 +0800

    add opt

---
+++ src/main/java/git/snippets/io/opt/connection/Constant.java
@@ -0,0 +1,9 @@
+package git.snippets.io.opt.connection;
+
+/**
+ * @author 闪电侠
+ */
+public class Constant {
+    static final int BEGIN_PORT = 8000;
+    static final int N_PORT = 100;
+}
---
+++ src/main/java/git/snippets/io/opt/thread/Constant.java
@@ -0,0 +1,8 @@
+package git.snippets.io.opt.thread;
+
+/**
+ * @author 闪电侠
+ */
+public class Constant {
+    public static final int PORT = 8000;
+}
