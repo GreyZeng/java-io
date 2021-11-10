8c0a646fd4b75924eb044e0d57ce677a8e8af29b
GreyZeng <410486047@qq.com>
2021-11-10 13:41:45 +0800

    io

--- src/main/java/git/snippets/io/TestByteBuffer.java
+++ src/main/java/git/snippets/io/TestByteBuffer.java
@@ -3,34 +3,34 @@ package git.snippets.io;
 import java.nio.ByteBuffer;
 
 public class TestByteBuffer {
-  public static void main(String[] args) {
-    ByteBuffer buffer = ByteBuffer.allocateDirect(1024);
-    //  ByteBuffer buffer = ByteBuffer.allocate(1024);
-    System.out.println("mark: " + buffer);
+    public static void main(String[] args) {
+        ByteBuffer buffer = ByteBuffer.allocateDirect(1024);
+        //  ByteBuffer buffer = ByteBuffer.allocate(1024);
+        System.out.println("mark: " + buffer);
 
-    buffer.put("123".getBytes());
+        buffer.put("123".getBytes());
 
-    System.out.println("-------------put:123......");
-    System.out.println("mark: " + buffer);
+        System.out.println("-------------put:123......");
+        System.out.println("mark: " + buffer);
 
-    buffer.flip(); // 读写交替
+        buffer.flip(); // 读写交替
 
-    System.out.println("-------------flip......");
-    System.out.println("mark: " + buffer);
+        System.out.println("-------------flip......");
+        System.out.println("mark: " + buffer);
 
-    buffer.get();
+        buffer.get();
 
-    System.out.println("-------------get......");
-    System.out.println("mark: " + buffer);
+        System.out.println("-------------get......");
+        System.out.println("mark: " + buffer);
 
-    buffer.compact();
+        buffer.compact();
 
-    System.out.println("-------------compact......");
-    System.out.println("mark: " + buffer);
+        System.out.println("-------------compact......");
+        System.out.println("mark: " + buffer);
 
-    buffer.clear();
+        buffer.clear();
 
-    System.out.println("-------------clear......");
-    System.out.println("mark: " + buffer);
-  }
+        System.out.println("-------------clear......");
+        System.out.println("mark: " + buffer);
+    }
 }
