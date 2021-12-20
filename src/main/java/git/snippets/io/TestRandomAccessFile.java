27615977a8c62639208806da6db8289ff9d8d100
GreyZeng <410486047@qq.com>
2021-12-20 23:31:23 +0800

    update code

--- src/main/java/git/snippets/io/TestRandomAccessFile.java
+++ src/main/java/git/snippets/io/TestRandomAccessFile.java
@@ -8,8 +8,13 @@ import static java.nio.charset.StandardCharsets.UTF_8;
 
 public class TestRandomAccessFile {
 
-    public static void main(String[] args) {
-        String path = "C:\\workspace\\out.txt";
+    public static void main(String[] args) throws Exception {
+        commonWrite();
+        channelWrite();
+    }
+
+    private static void commonWrite() {
+        String path = "C:\\git\\out.txt";
         try {
             RandomAccessFile randomAccessFile = new RandomAccessFile(path, "rw");
             randomAccessFile.write("Hello xxxld".getBytes(UTF_8));
@@ -21,14 +26,18 @@ public class TestRandomAccessFile {
     }
 
 
-    public static void testRandomAccessFileIO() throws Exception {
-        String path = "C:\\workspace\\out.txt";
+    public static void channelWrite() throws Exception {
+        String path = "C:\\git\\out.txt";
         byte[] data = "1234567\n".getBytes();
         RandomAccessFile file = new RandomAccessFile(path, "rw");
         FileChannel channel = file.getChannel();
         int size = 4096 * 100;
+        // 只有文件（块设备）才会做内存映射，才会有map方法
+        // 堆外的和文件的bytebuffer
         MappedByteBuffer map = channel.map(FileChannel.MapMode.READ_WRITE, 0, size);
         while (size != 0) {
+            // 不是系统调用 但是数据会到达内核的pagecache
+            // 曾经我们需要用out.write()这样的系统调用才能让数据到达内核的pagecache（必须需要内核到用户态切换）
             map.put(data);
             size -= data.length;
         }
